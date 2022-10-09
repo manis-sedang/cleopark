86fe916a947a76f696f9694a55647c18448af1f7
manis-sedang <gevelinanesta46@gmail.com>
2022-10-09 09:58:18 +0000

    cleopark

---
+++ spec/cleopark-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CleoparkView from '../lib/cleopark-view';
+
+describe('CleoparkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
